61cb71cc5c48f8d818c80b70c9a0edf4ec05a580
Your Name <you@example.com>
2021-08-28 23:21:52 -0300

    cambio icono a menu con css

--- codigo.js
+++ codigo.js
@@ -50,20 +50,5 @@ seleccionSeccion=()=>{
 }
 
 
-tamañoSecciones=()=>{
-   
-        /*
-        El 40 es del weight del nav
-        */ 
-         const inicio= document.getElementById("contenedor_inicio");
-         const s1= document.getElementById("contenedor_sobremi");
-         const s2= document.getElementById("contenedor_sobremi2");
-         const s3= document.getElementById("contenedor_contacto");
 
-
-         inicio.style.height=alto-40+"px";
-         s1.style.height=alto+"px";
-         s2.style.height=alto+"px";
-         s3.style.height=alto+"px";
-}
 
